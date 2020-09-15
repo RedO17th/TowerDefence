c4c8aec2347f84502498739836397d265c1f8d25
RedO17th <saratovcev-1995@mail.ru>
2020-09-15 18:40:13 +0300

    CommitOne

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    [SerializeField] GameObject btn;
+    void Awake()
+    {
+        btn.transform.GetComponent<Button>().onClick.AddListener(ExitGame);
+    }
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
